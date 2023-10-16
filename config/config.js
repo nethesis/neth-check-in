5edebc824f71c5e6ffc0ee826a79e2845060400d
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-16 11:42:19 +0200

    feat: added containerization

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 var CONFIGS = {
     MYSQL_DB_NAME: 'nethcheckin',
-    MYSQL_DB_HOST: 'localhost',
+    MYSQL_DB_HOST: 'db',
     MYSQL_DB_PORT: 3306,
-    MYSQL_DB_USERNAME: 'sebastian',
-    MYSQL_DB_PASSWORD: 'sebastian',
-    IP: '172.25.5.78',
+    MYSQL_DB_USERNAME: 'nethcheckin',
+    MYSQL_DB_PASSWORD: 'nethcheckin',
+    IP: '127.0.0.1',
     NODEJS_PORT: 8080
 };
 
